8229ac0767d79ab3ded74d73d034169bdf33159e
Evasive-6 <albinshiju285@gmail.com>
2025-01-21 14:55:35 +0530

    fixed

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT=8884;
+const PORT=8886;
 
 app.get("/ping",(req,res)=>{
     res.send("pong")
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`)
 })
